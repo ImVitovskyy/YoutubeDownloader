f6ab91f378ec0711e3ea45f81048101e6b55c8c1
Vitovskyy <97605890+ImVitovskyy@users.noreply.github.com>
2022-12-28 17:17:44 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,29 +3,18 @@ from pytube import Playlist
 from moviepy.editor import *
 import click
 
-helpPage = ('Hello and welcome to YoutubeDownloader!\n\n'
-    'Here, you can download videos, music, and even entire playlists from Youtube without ads and in a totally transparent way, as you can see in our Github repository: https://github.com/ImVitovskyy/YoutubeDonloader\n\n'
-    'Please note that this project is for EDUCATIONAL PURPOSES ONLY! '
-    'The main idea of the project is to de-popularize services that download videos from Youtube but actually hide malicious scripts in their tools. '
-    'Here, you have access to the entire repository, where you can follow and especially STUDY the Python language and some of its tools, seeing that it is possible to do the same thing as those malicious tools, but in a completely safe and open source way.\n\n'
-    'Do not use the offered content for commercialization, advertising, or any other illegal distribution. '
-    'BEWARE! The downloaded content may contain copyright and we are not responsible for any judicial actions you may suffer by using these protected content in the wrong way.\n'
-    'It is important to respect the rights of content creators and the community of the platform. '
-    'This means following the terms of service and respecting copyright laws. '
-    'It is essential to give credit to the creators of the content and not use their work without permission. '
-    'Again, this project is for EDUCATIONAL PURPOSES ONLY! '
-    'Feel free to study, ask questions, and suggest changes to it on Github, as long as you do not violate the PROJECT LICENSE!\n\n'
-    'Run the service with "python main.py" and follow the instructions given throughout the script.')
+with open('./help.txt', encoding='utf-8', mode='r') as f:
+    helpPage = f.read()
     
 @click.command(help=helpPage)
 
 def getContent(): 
     print(
         '\n'
-        '|           Welcome to Youtube Downloader!          |\n'
-        '|---------------------------------------------------|\n'
-        '|  https://github.com/ImVitovskyy/YoutubeDonloader  |\n'
-        '|---------------------------------------------------|\n\n')
+        '|            Welcome to Youtube Downloader!          |\n'
+        '|----------------------------------------------------|\n'
+        '|  https://github.com/ImVitovskyy/YoutubeDownloader  |\n'
+        '|----------------------------------------------------|\n\n')
 
     url = input(
         'First, paste the video, shorts or playlist URL here:\n'
